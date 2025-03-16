5f4d921ccd16ff8b2bda8ab6f4ff90ecce08a148
Liudmyla Ilchenko <116093817+liudmyla-ilchenko@users.noreply.github.com>
2025-03-16 13:03:36 +0200

    hw 5 / task 2

---
+++ task_02.py
@@ -0,0 +1,77 @@
+import string
+from concurrent.futures import ThreadPoolExecutor
+from collections import defaultdict
+import requests
+import heapq
+import matplotlib.pyplot as plt
+
+
+def get_text(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.text
+    except requests.RequestException as e:
+        return None
+
+
+def remove_punctuation(text):
+    return text.translate(str.maketrans("", "", string.punctuation))
+
+
+def map_function(word):
+    return word, 1
+
+
+def shuffle_function(mapped_values):
+    shuffled = defaultdict(list)
+    for key, value in mapped_values:
+        shuffled[key].append(value)
+    return shuffled.items()
+
+
+def reduce_function(key_values):
+    key, values = key_values
+    return key, sum(values)
+
+
+def map_reduce(text, search_words=None):
+    # Видалення знаків пунктуації
+    text = remove_punctuation(text)
+    words = text.split()
+
+    # Якщо задано список слів для пошуку, враховувати тільки ці слова
+    if search_words:
+        words = [word for word in words if word in search_words]
+
+    # Паралельний Мапінг
+    with ThreadPoolExecutor() as executor:
+        mapped_values = list(executor.map(map_function, words))
+
+    # Крок 2: Shuffle
+    shuffled_values = shuffle_function(mapped_values)
+
+    # Паралельна Редукція
+    with ThreadPoolExecutor() as executor:
+        reduced_values = list(executor.map(reduce_function, shuffled_values))
+
+    return dict(reduced_values)
+
+
+if __name__ == "__main__":
+    url = "https://gutenberg.net.au/ebooks01/0100021.txt"
+    text = get_text(url)
+    if text:
+        result = map_reduce(text)
+
+        top_words = heapq.nlargest(10, result.items(), key=lambda item: item[1])
+        words, counts = zip(*top_words)
+
+        plt.bar(words, counts)
+        plt.xlabel("Words")
+        plt.ylabel("Count")
+        plt.title("Top 10 words")
+        plt.show()
+
+    else:
+        print("Error fetching text.")
